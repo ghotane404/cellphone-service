420363d1488c2443bc94ca45cdc4e756acf49759
ghotane404 <62863408+ghotane404@users.noreply.github.com>
2026-04-15 15:30:59 -0700

    in case of needing to rollback

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -25,13 +25,31 @@ public class CellPhoneApplication {
         // creates a new CellPhone object and stores it in the variable cellPhone
         // sends all user input value to the constructor
         CellPhone cellPhone = new CellPhone(serialNumber, model, carrier, phoneNumber, owner);
+        printPhoneData(cellPhone);  // creating method for exercise 1 and less messy?
+        display (cellPhone);
+//        display(cellPhone1);
+//        display(cellPhone2);
+    }
 
+    public static void display(CellPhone phone) {
+        System.out.println(phone.getPhoneNumber());
+//        phone.getOwner();
+//        phone.getPhoneNumber();
+
+    }
+
+    public static void collectUserInput(){
+
+    }
+
+    public static void printPhoneData(CellPhone cellPhone) {
         // cellPhone.getSerialNumber() means Java goes into that object, reads its serialNumber and returns the value
-        System.out.println("What is the serial number? " + cellPhone.getSerialNumber());
+        System.out.println("\nWhat is the serial number? " + cellPhone.getSerialNumber());
         System.out.println("What model is the phone? " + cellPhone.getModel());
         System.out.println("Who is the carrier? " + cellPhone.getCarrier());
         System.out.println("What is the phone number? " + cellPhone.getPhoneNumber());
         System.out.println("Who is the owner of the phone? " + cellPhone.getOwner());
+        System.out.println();
         // (side note for ME: kind of like SQL query with table name cellPhone and .getXYZ is the column in the table)
     }
 }
