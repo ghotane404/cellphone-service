5f32e889e2f02fe37c586cc518873c60b585dc31
ghotane404 <62863408+ghotane404@users.noreply.github.com>
2026-04-15 12:59:18 -0700

    update code and added comments

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,67 @@
 package com.pluralsight;
 
 public class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+//    Add a parameterless constructor (a constructor without parameters). Provide the following default values for all string data types in the constructor.
+//    public CellPhone() {
+//        this.serialNumber = 0;
+//        this.model = "";
+//        this.carrier = "";
+//        this.phoneNumber = "";
+//        this.owner = "";
+//    }
+
+    // this is a constructor- runs when a new CellPhone object is created
+    // it receives the values passed from main()
+    public CellPhone (int serialNumber,  String model, String carrier, String phoneNumber, String owner) {
+        // this.serialNumber = the object’s field
+        // serialNumber = the constructor parameter
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+    }
+
+    public int getSerialNumber() {    // getter for serialNumber
+        return serialNumber;    // returns the serial number stored in the object
+    }
+    public void setSerialNumber(int serialNumber) { // setter for serialNumber
+        this.serialNumber = serialNumber;   // stores the new serial number in the object
+        // ex. cellPhone.setSerialNumber(1234);
+        // 	replaces the old serial number with 1234
+    }
+
+    public String getModel() {  // getter for getModel
+        return model;
+    }
+    public void setModel(String model) { // setter for setModel
+        this.model = model;
+    }
+
+    public String getCarrier() {  // getter for getCarrier
+        return carrier;
+    }
+    public void setCarrier(String carrier) { // setter for setCarrier
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {  // getter for getPhoneNumber
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) { // setter for setPhoneNumber
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {  // getter for getOwner
+        return owner;
+    }
+    public void setOwner(String owner) { // setter for setOwner
+        this.owner = owner;
+    }
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
+import java.util.*;
 
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        int serialNumber;
+        String model;
+        String carrier;
+        String phoneNumber;
+        String owner;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is the serial number? ");
+        serialNumber = scanner.nextInt();
+        scanner.nextLine(); // clears the extra newline after nextInt()
+        System.out.println("What model is the phone? ");
+        model = scanner.nextLine();
+        System.out.println("Who is the carrier? ");
+        carrier = scanner.nextLine();
+        System.out.println("What is the phone number? ");
+        phoneNumber = scanner.nextLine();
+        System.out.println("Who is the owner of the phone? ");
+        owner = scanner.nextLine();
+
+        // it's creating a new CellPhone() object and calls it cellPhone
+        // sends all user input value to the constructor
+        CellPhone cellPhone = new CellPhone(serialNumber, model, carrier, phoneNumber, owner);
+
+        // cellPhone.getSerialNumber() means Java goes into that object, reads its serialNumber and returns the value
+        System.out.println("What is the serial number? " + cellPhone.getSerialNumber());
+        System.out.println("What model is the phone? " + cellPhone.getModel());
+        System.out.println("Who is the carrier? " + cellPhone.getCarrier());
+        System.out.println("What is the phone number? " + cellPhone.getPhoneNumber());
+        System.out.println("Who is the owner of the phone? " + cellPhone.getOwner());
+    }
 }
